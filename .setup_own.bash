230ac4fabe9f55370356b308e0ccd9f1b199a4ba
makotoyoshigoe <myoshigo0127@gmail.com>
2025-09-02 17:05:42 +0900

    add: setup script

--- .setup_own.bash
+++ .setup_own.bash
@@ -1,4 +1,13 @@
 #!/usr/bash
-pkg_name=$1
-echo $pkg_name
-
+new_name=$1
+echo "new package name: $new_name"
+name_xml=`echo "cat /package/name" | xmllint --shell package.xml`
+org_name=`echo ${name_xml} | sed -e "s/^.*<name.*>\(.*\)<\/name>.*$/\1/"`
+echo "current package name: $org_name"
+if [ $org_name == $new_name ]; then
+	echo "new package name equal original package name. "
+else
+	mv ../$org_name ../$new_name && cd ../$new_name
+	mv include/$org_name include/$new_name
+	sed -i "s/${org_name}/${new_name}/" CMakeLists.txt package.xml
+fi
